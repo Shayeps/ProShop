f272c00f3b47c08343932c3ee9ef908a872dd5e2
Shay Epstein <shaye@brightdata.local>
2021-11-16 23:24:37 +0200

    added backend

--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 import Product from '../../components/Product';
-import products from '../../products';
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+
+            setProducts(data)
+        }
+
+        fetchProducts();
+    }, []);
+
     return (
         <>
-        <h1>Latest Products</h1>
-        <Row>
-            {products.map(p => (
-                <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={p}/>
-                </Col>
-            ))}
-        </Row>
-    </>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map(p => (
+                    <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={p} />
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
