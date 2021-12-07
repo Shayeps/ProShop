72cde7be40989408f5c0692c9a5c29ee0914b748
Shay Epstein <shaye@brightdata.local>
2021-12-07 22:29:04 +0200

    added redux, loadin spinner and error message

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // @access  public
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({});
-
+    
     res.json(products)
 }));
 
--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
 import Product from '../../components/Product';
+import { listProducts } from '../../actions/productActions';
+import Spinner from '../loader';
+import Message from '../message';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const { loading, error, products } = useSelector(state => state.productList);
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-
-            setProducts(data)
-        }
-
-        fetchProducts();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
 
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(p => (
-                    <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={p} />
-                    </Col>
-                ))}
-            </Row>
+            {
+                loading ? <Spinner /> :
+                    error ? <Message variant='danger' >{error}</Message> :
+                        <Row>
+                            {products.map(p => (
+                                <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={p} />
+                                </Col>
+                            ))}
+                        </Row>
+            }
+
         </>
     )
 }
